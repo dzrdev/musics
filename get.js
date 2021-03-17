47b63a0b1ff1848eb75248577ff876adfb9bb5cb
4tangsusu <67858918+4tangsusu@users.noreply.github.com>
2021-03-17 14:28:54 +0900

    Update get.js

--- get.js
+++ get.js
@@ -23,3 +23,18 @@ document.querySelector('.entryPauseButtonMinimize').addEventListener('click', e 
         play();
     }
 });
+let btn = document.createElement('button');
+btn.innerHTML = '음원 다운로드';
+btn.addEventListener('click', e => {
+    window.open(file.src, '_blank');
+});
+//button css code
+let css = btn.style;
+css.color = 'white';
+css.backgroundColor = 'rgb(22, 216, 163)';
+css.border = 0;
+css.borderRadius = '4px';
+css.fontSize = '1em';
+css.padding = '9px 20px';
+css.marginLeft = '10px';
+document.querySelector('.css-bl8trf').appendChild(btn);
